b3f6c23e4810c4c2cbe5c480bdedc00b23f9cfd2
James Smith <james.smith9113@gmail.com>
2019-07-16 15:37:12 -0400

    Initial commit after CMS Cookiecutter creation, version 1.0

---
+++ GreensFunction/tests/test_GreensFunction.py
@@ -0,0 +1,12 @@
+"""
+Unit and regression test for the GreensFunction package.
+"""
+
+# Import package, test suite, and other packages as needed
+import GreensFunction
+import pytest
+import sys
+
+def test_GreensFunction_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "GreensFunction" in sys.modules
